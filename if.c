f43d1ef5cf1746abaed173b65f6c8ce708c33682
victor4500 <sla133278@gmail.com>
2026-04-19 23:58:10 -0300

    poucas mudanças no if

--- if.c
+++ if.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
 int main (){
-    int idade = 18;
+    printf("digite sua idade:\n ");
+    int idade;
+    
+    scanf("%d",  &idade);
+
     if (idade >= 18)
     {
         printf("pode passar fi\n");
